5f180b76848a442d5023cb3c61b39ee196ad4e6a
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-05-19 16:41:18 +0200

    ➕ Ajout resto.php & includes/resto.php 🛠 Mise à jour style.css

---
+++ includes/resto.php
@@ -0,0 +1,159 @@
+<div class="container">
+        <div class="card" style="border: none;">
+            <div class="card-body" style="padding-left: 0;">
+            <p><a href="#">Chemin</a>><a href="#">d'accès</a>><a href="#">...</a></p>
+                <h4 class="card-title">Nom du restaurant </h4>
+                
+                <!-- AddToAny BEGIN -->
+                <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+                <a class="a2a_button_facebook"></a>
+                <a class="a2a_button_twitter"></a>
+                <a class="a2a_button_whatsapp"></a>
+                <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+                </div>
+                <script>
+                var a2a_config = a2a_config || {};
+                a2a_config.locale = "fr-FR";
+                </script>
+                <script async src="assets/js/addtoany.js"></script>
+                <!-- AddToAny END -->
+
+                <br>
+                <h6 class="text-muted card-subtitle mb-2">Fiche mise à jour le JJ/MM/AAAA - Restaurateur, mettez à jours vos documents <a href="#">ici</a></h6>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="description-slider">
+        <div class="row">
+            <div id="description-col" class="col" style="display: flex; max-width:50%;">
+                <div class="card" style="border: none;">
+                    <div class="card-body" style="padding: 0;">
+                        <img class ="card-hearts" src="assets/img/a-la-folie.gif">
+                        <h5 class="card-title">Courte description</h5>
+                        <button class="btn btn-primary" style="margin-bottom:10px; border-color: #990000; background-color: #ff9966;"><i class="fas fa-phone-alt" style="color: #990000;"></i> Réserver</button>
+                        <h4 class="card-title">Menu de XX€ à XX€</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col col-custom-slider" style="max-width:50%;">
+                <ul id="customize">
+                    <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
+                    <li><img src="https://www.nottingham.ac.uk/Home/FeatureEvents/2018/Images/power-of-pictures-920.jpg"></li>
+                    <li><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg"></li>
+                    <li><img src="https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"></li>
+                    <li><img src="https://images.unsplash.com/photo-1469827160215-9d29e96e72f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></li>
+                    <li><img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEXVRyV72gzlBr6oMsP9PuJ2YL17yLcjty9gjyj_-IY9c6Q8Lg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQvn8jov5i7u0z7Cd98SbJ8GRSpdUPnP5baOvaP2jOfGNE0f7"></li>
+                    <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
+                    <li><img src="https://www.nottingham.ac.uk/Home/FeatureEvents/2018/Images/power-of-pictures-920.jpg"></li>
+                    <li><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg"></li>
+                    <li><img src="https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"></li>
+                    <li><img src="https://images.unsplash.com/photo-1469827160215-9d29e96e72f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></li>
+                    <li><img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEXVRyV72gzlBr6oMsP9PuJ2YL17yLcjty9gjyj_-IY9c6Q8Lg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQvn8jov5i7u0z7Cd98SbJ8GRSpdUPnP5baOvaP2jOfGNE0f7"></li>
+                </ul>
+                <ul id="customize-thumbnails">
+                    <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
+                    <li><img src="https://www.nottingham.ac.uk/Home/FeatureEvents/2018/Images/power-of-pictures-920.jpg"></li>
+                    <li><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg"></li>
+                    <li><img src="https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"></li>
+                    <li><img src="https://images.unsplash.com/photo-1469827160215-9d29e96e72f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></li>
+                    <li><img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEXVRyV72gzlBr6oMsP9PuJ2YL17yLcjty9gjyj_-IY9c6Q8Lg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQvn8jov5i7u0z7Cd98SbJ8GRSpdUPnP5baOvaP2jOfGNE0f7"></li>
+                    <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
+                    <li><img src="https://www.nottingham.ac.uk/Home/FeatureEvents/2018/Images/power-of-pictures-920.jpg"></li>
+                    <li><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg"></li>
+                    <li><img src="https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"></li>
+                    <li><img src="https://images.unsplash.com/photo-1469827160215-9d29e96e72f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></li>
+                    <li><img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEXVRyV72gzlBr6oMsP9PuJ2YL17yLcjty9gjyj_-IY9c6Q8Lg"></li>
+                    <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQvn8jov5i7u0z7Cd98SbJ8GRSpdUPnP5baOvaP2jOfGNE0f7"></li>
+                </ul>
+                <ul id="customize-controls">
+                    <li class="prev" aria-controls="customize" tabindex="-1" data-controls="prev">
+                        <img src="https://ganlanyuan.github.io/tiny-slider/demo/images/angle-left.png">
+                    </li>
+                    <li class="next" aria-controls="customize" tabindex="-1" data-controls="next">
+                        <img src="https://ganlanyuan.github.io/tiny-slider/demo/images/angle-right.png">
+                    </li>
+                </ul>
+                <script type="module">
+                    var slider = tns({
+                        container: "#customize",
+                        navContainer: "#customize-thumbnails",
+                        items: 1,
+                        navAsThumbnails: true,
+                        loop: true,
+                        fixedWidth: 500,
+                        nav: true,
+                        controls: true,
+                        controlsContainer: "#customize-controls"
+                    });
+                    $('#customize-thumbnails li').each((_index, element)=>{
+                        $(element).hover(()=>{
+                            slider.goTo($(element).attr("data-nav"));
+                        });
+                        $(element).click(()=>{
+                            $('body').append(`
+                                <div class="custom-modal">
+                                    <div class="custom-modal-container">
+                                        <button style="position: absolute; right: 15px;">X</button>
+                                        <div class="modal-content">
+                                            <ul id="modal-slider">
+                                                <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
+                                                <li><img src="https://www.nottingham.ac.uk/Home/FeatureEvents/2018/Images/power-of-pictures-920.jpg"></li>
+                                                <li><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/2E2E/production/_102622811_sunrise.jpg"></li>
+                                                <li><img src="https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"></li>
+                                                <li><img src="https://images.unsplash.com/photo-1469827160215-9d29e96e72f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"></li>
+                                                <li><img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg"></li>
+                                                <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEXVRyV72gzlBr6oMsP9PuJ2YL17yLcjty9gjyj_-IY9c6Q8Lg"></li>
+                                                <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyQvn8jov5i7u0z7Cd98SbJ8GRSpdUPnP5baOvaP2jOfGNE0f7"></li>
+                                            </ul>
+                                            <ul id="modal-customize-controls">
+                                                <li class="prev" aria-controls="customize" tabindex="-1" data-controls="prev">
+                                                    <img src="https://ganlanyuan.github.io/tiny-slider/demo/images/angle-left.png">
+                                                </li>
+                                                <li class="next" aria-controls="customize" tabindex="-1" data-controls="next">
+                                                    <img src="https://ganlanyuan.github.io/tiny-slider/demo/images/angle-right.png">
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            `);
+                            $('div.custom-modal button').click(()=>{
+                                $('div.custom-modal').remove();
+                            });
+                            var second_slider = tns({
+                                container: "#modal-slider",
+                                fixedWidth: 1000,
+                                items: 1,
+                                loop: true,
+                                controls: true,
+                                nav: false,
+                                controlsContainer: "#modal-customize-controls",
+                            });
+                        });
+                    });
+                    console.log(slider);
+                </script>
+            </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="intro">
+                    <h2 class="text-left">Pour voir la carte,...</h2>
+                    <form class="form-inline d-xl-flex" method="post" action="resto_connected.php">
+                        <div class="form-group"><input class="form-control" type="email" placeholder="Votre adresse email ici ..." name="email"></div><button class="btn btn-primary" type="submit" style="background-color:#ff9966; border: 1px solid #990000;">Inscription&nbsp;/ Connexion&nbsp;</button>
+                    </form>
+                    <p class="text-left" style="margin-top: 10px; font-size: 18px;">Votre adresse email ne sera pas vendue sans autorisation prélable de votre part.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,3 +1,4 @@
 <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/map.js"></script>
+    <script src="assets/tiny-slider/js/tiny-slider.js"></script>
