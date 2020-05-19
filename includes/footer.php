7aa1332833f2deb21ac4c8e158876072357d9b89
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-05-19 13:02:26 +0200

    Ajout home.php & footer.php

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+<footer class="text-center d-xl-flex justify-content-xl-end align-items-xl-center footer bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 d-xl-flex my-auto justify-content-xl-start h-100 text-center text-lg-left">                <div id="stats">
+                    <h2>Statistiques</h2>
+                    <br>
+                        - 335 891 membres<br />
+                        - 175 990 membres actifs<br />
+                        - 36 893 livres d'or<br>
+                        - 18 991 restaurants<br />
+                        - 69 304 pages scannées<br>
+                        - 1 918 nouveaux menus<br />
+                        - 7 047 nouvelles pages
+                </div></div>
+                <div class="col d-xl-flex justify-content-xl-end">
+                    <ul class="text-right" style="list-style: none;">
+                        <li><a href="#" style="color: black">Mentions Légales</a></li>
+                        <li><a href="#" style="color: black">CGU</a></li>
+                        <li><a href="#" style="color: black">RGPD</a></li>
+                        <li><a href="#" style="color: black">Partenaires</a></li>
+                    </ul>
+                </div>
+            </div>
+            <p>Copyright © 2020 Le Club des Bons Vivants - <a href="https://alexandre-leclerc.fr">Alexandre Leclerc</a></p>
+        </div>
+</footer>
