63347c62ae7681c773126510e10df5ebe2441bba
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-05-19 16:28:51 +0200

    🛠 Mise à jour futile

--- includes/list-resto.php
+++ includes/list-resto.php
@@ -2,41 +2,41 @@
 <p><a href="#">Chemin</a>><a href="#">d'accès</a>><a href="#">...</a></p>
 <h2> XX Departments </h2>
 </div>
-    <hr>
-    <div class="container">
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Nom</th>
-                        <th>Adresse</th>
-                        <th>Prix</th>
-                        <th>Vote / Avis</th>
-                        <th>Genre</th>
-                        <th>Ville</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><img class="miniature" src="#"></td>
-                        <td><a href="resto.php" style="color: black;">Resto1</a></td>
-                        <td>Adresse</td>
-                        <td>XX €</td>
-                        <td><img class="list-hearts" src="assets/img/a-la-folie.gif"> (X avis)</td>
-                        <td>Genre</td>
-                        <td>Ville</td>
-                    </tr>
-                    <tr>
-                        <td><img class="miniature" src="#"></td>
-                        <td><a href="#" style="color: black;">Resto2</a></td>
-                        <td>Adresse</td>
-                        <td>XX €</td>
-                        <td><img class="list-hearts" src="assets/img/beaucoup.gif"> (X avis)</td>
-                        <td>Genre</td>
-                        <td>Ville</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+<hr>
+<div class="container">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Nom</th>
+                    <th>Adresse</th>
+                    <th>Prix</th>
+                    <th>Vote / Avis</th>
+                    <th>Genre</th>
+                    <th>Ville</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img class="miniature" src="#"></td>
+                    <td><a href="resto.php" style="color: black;">Resto1</a></td>
+                    <td>Adresse</td>
+                    <td>XX €</td>
+                    <td><img class="list-hearts" src="assets/img/a-la-folie.gif"> (X avis)</td>
+                    <td>Genre</td>
+                    <td>Ville</td>
+                </tr>
+                <tr>
+                    <td><img class="miniature" src="#"></td>
+                    <td><a href="#" style="color: black;">Resto2</a></td>
+                    <td>Adresse</td>
+                    <td>XX €</td>
+                    <td><img class="list-hearts" src="assets/img/beaucoup.gif"> (X avis)</td>
+                    <td>Genre</td>
+                    <td>Ville</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+</div>
