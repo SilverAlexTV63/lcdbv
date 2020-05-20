4a2e035109fcab402ab9cf2fd553486042bee0d7
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-05-20 10:35:17 +0200

    ➕ Ajout de resto-full-display.php & includes/resto-fill-display.php 🛠� Modification includes/resto.php

--- includes/resto-full-display.php
+++ includes/resto-full-display.php
@@ -2,7 +2,7 @@
         <div class="card" style="border: none;">
             <div class="card-body" style="padding-left: 0;">
             <p><a href="#">Chemin</a>><a href="#">d'accès</a>><a href="#">...</a></p>
-                <h4 class="card-title">Nom du restaurant</h4>
+                <h4 class="card-title">Nom du restaurant </h4>
                 <!-- AddToAny BEGIN -->
                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
                 <a class="a2a_button_facebook"></a>
@@ -25,7 +25,7 @@
         <div class="row">
             <div id="description-col" class="col" style="display: flex; max-width:50%;">
                 <div class="card" style="border: none;">
-                    <div class="card-body" style="padding: 0;">
+                    <div class="card-body" style="padding-left: 0;">
                         <img class ="card-hearts" src="assets/img/a-la-folie.gif">
                         <h5 class="card-title">Courte description</h5>
                         <button class="btn btn-primary" style="margin-bottom:10px; border-color: #990000; background-color: #ff9966;"><i class="fas fa-phone-alt" style="color: #990000;"></i> Réserver</button>
@@ -98,7 +98,7 @@
                             $('body').append(`
                                 <div class="custom-modal">
                                     <div class="custom-modal-container">
-                                        <button class="btn-primary" style="text-align: center; line-height: 30px; font-size: 24px; position: absolute; top: 10px; right: 30px; background-color: #ff9966; border: 1px solid #990000; border-radius: .25rem;">X</button>
+                                        <button style="position: absolute; right: 15px;">X</button>
                                         <div class="modal-content">
                                             <ul id="modal-slider">
                                                 <li><img src="https://static.toiimg.com/thumb/58475411/Kolkata-in-pictures.jpg?width=748&height=499"></li>
@@ -140,18 +140,257 @@
                 </script>
             </div>
     </div>
+</div>
+<style>
+    a {
+  color: #92badd;
+  display:inline-block;
+  text-decoration: none;
+  font-weight: 400;
+}
+
+h2 {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  display:inline-block;
+  margin: 40px 8px 10px 8px;
+  color: #cccccc;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  min-height: 100%;
+  padding: 20px;
+}
+
+#formContent {
+  -webkit-border-radius: 10px 10px 10px 10px;
+  border-radius: 10px 10px 10px 10px;
+  background: #fff;
+  padding: 30px;
+  width: 90%;
+  max-width: 450px;
+  position: relative;
+  padding: 0px;
+  -webkit-box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+  box-shadow: 0 10px 20px 0 rgba(0,0,0,0.3);
+  text-align: center;
+}
+
+#formFooter {
+  background-color: #f6f6f6;
+  border-top: 1px solid #dce8f1;
+  padding: 25px;
+  text-align: center;
+  -webkit-border-radius: 0 0 10px 10px;
+  border-radius: 0 0 10px 10px;
+}
+
+input[type=button], input[type=submit], input[type=reset] {
+  background-color: #ff9966;
+  border: 1px solid #990000;
+  color: white;
+  padding: 3% 5%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 13px;
+  -webkit-box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
+  box-shadow: 0 10px 30px 0 rgba(95,186,233,0.4);
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+  margin: 5px 20px 40px 20px;
+  -webkit-transition: all 0.2s ease-in-out;
+  -moz-transition: all 0.2s ease-in-out;
+  -ms-transition: all 0.2s ease-in-out;
+  -o-transition: all 0.2s ease-in-out;
+  transition: all 0.2s ease-in-out;
+}
+
+input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover {
+  background-color: #990000;
+  boder: solid #ff9966;
+}
+
+input[type=text] {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 5px;
+  width: 85%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.4s ease-in-out;
+  -moz-transition: all 0.4s ease-in-out;
+  -ms-transition: all 0.4s ease-in-out;
+  -o-transition: all 0.4s ease-in-out;
+  transition: all 0.4s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+
+input[type=text]:focus {
+  background-color: #fff;
+  border-bottom: 2px solid #ff9966;
+}
+
+input[type=text]:focus::placeholder {
+  opacity: 0;
+}
+
+input[type=text]:placeholder {
+  color: #cccccc;
+}
+
+/* Simple CSS3 Fade-in-down Animation */
+.fadeInDown {
+  -webkit-animation-name: fadeInDown;
+  animation-name: fadeInDown;
+  -webkit-animation-duration: 0.5s;
+  animation-duration: 0.5s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+@-webkit-keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: none;
+    transform: none;
+  }
+}
+
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    -webkit-transform: translate3d(0, -100%, 0);
+    transform: translate3d(0, -100%, 0);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: none;
+    transform: none;
+  }
+}
+
+@-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+
+.fadeIn {
+  opacity:0;
+  -webkit-animation:fadeIn ease-in 1;
+  -moz-animation:fadeIn ease-in 1;
+  animation:fadeIn ease-in 1;
+
+  -webkit-animation-fill-mode:forwards;
+  -moz-animation-fill-mode:forwards;
+  animation-fill-mode:forwards;
+
+  -webkit-animation-duration:1s;
+  -moz-animation-duration:1s;
+  animation-duration:1s;
+}
+
+.fadeIn.first {
+  -webkit-animation-delay: 0.4s;
+  -moz-animation-delay: 0.4s;
+  animation-delay: 0.4s;
+}
+
+.fadeIn.second {
+  -webkit-animation-delay: 0.6s;
+  -moz-animation-delay: 0.6s;
+  animation-delay: 0.6s;
+}
+
+.fadeIn.third {
+  -webkit-animation-delay: 0.8s;
+  -moz-animation-delay: 0.8s;
+  animation-delay: 0.8s;
+}
+
+.fadeIn.fourth {
+  -webkit-animation-delay: 1s;
+  -moz-animation-delay: 1s;
+  animation-delay: 1s;
+}
+
+.underlineHover:after {
+  display: block;
+  left: 0;
+  bottom: -10px;
+  width: 0;
+  height: 2px;
+  background-color: #56baed;
+  content: "";
+  transition: width 0.2s;
+}
+
+.underlineHover:hover {
+  color: #0d0d0d;
+}
+
+.underlineHover:hover:after{
+  width: 100%;
+}
+
+*:focus {
+    outline: none;
+}
+
+#icon {
+  width:60%;
+}
+</style>
+<div class="comment-redaction">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="intro">
-                    <h2 class="text-left">Pour voir la carte,...</h2>
-                    <form class="form-inline d-xl-flex" method="post" action="resto-full-display.php">
-                        <div class="form-group"><input class="form-control" type="email" placeholder="Votre adresse email ici ..." name="email"></div><button class="btn btn-primary" type="submit" style="background-color:#ff9966; border: 1px solid #990000;">Inscription&nbsp;/ Connexion&nbsp;</button>
-                    </form>
-                    <p class="text-left" style="margin-top: 10px; font-size: 18px;">Votre adresse email ne sera pas vendue sans autorisation prélable de votre part.</p>
-                </div>
-            </div>
-        </div>
+        <h4 id="comment-redacion">Rédigez votre avis</h4>
+        <form action="" method="post" id="redaction">
+            <label for="visit-date">Date de visite (JJ.MM.AA)</label>
+            <input type="text" name="visit-date" id="visit-date" placeholder="JJ.MM.AA">
+        </form>
     </div>
 </div>
+<div id="comments">
+    <div class="container comment">
+        <h5 class="username">fagenrus</h5>
+        <p class="comment-date">09/05/2020</p>
+        <img class="hearts" src="assets/img/j-aime-a-la-folie.gif">
+        <p class="comment-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt tincidunt nulla. Donec tristique nibh ac
+            velit laoreet, et accumsan lectus volutpat. Quisque eget urna turpis. Suspendisse elit sapien, faucibus nec augue at, imperdiet blandit lectus.
+            In hac habitasse platea dictumst. Sed ullamcorper nisl in placerat convallis. Ut non elit et mi feugiat lobortis. Maecenas porttitor dignissim dictum.
+            Duis laoreet nisi vel metus tempor facilisis. Etiam ac ipsum dolor. Nam gravida sagittis urna, et euismod orci euismod sit amet.
+            Nulla congue purus ac risus semper semper. Duis ex lacus, elementum a porta eget, vestibulum ac tortor. Praesent nec dolor nulla.
+            Nam sed mauris felis.</p>
+        <i class="fas fa-reply" title="Réservé aux restaurateurs"></i>
+    </div>
+    <div class="container comment">
+        <h5 class="username">fagenrus</h5>
+        <p class="comment-date">09/05/2020</p>
+        <img class="hearts" src="assets/img/j-aime-a-la-folie.gif">
+        <p class="comment-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt tincidunt nulla. Donec tristique nibh ac
+            velit laoreet, et accumsan lectus volutpat. Quisque eget urna turpis. Suspendisse elit sapien, faucibus nec augue at, imperdiet blandit lectus.
+            In hac habitasse platea dictumst. Sed ullamcorper nisl in placerat convallis. Ut non elit et mi feugiat lobortis. Maecenas porttitor dignissim dictum.
+            Duis laoreet nisi vel metus tempor facilisis. Etiam ac ipsum dolor. Nam gravida sagittis urna, et euismod orci euismod sit amet.
+            Nulla congue purus ac risus semper semper. Duis ex lacus, elementum a porta eget, vestibulum ac tortor. Praesent nec dolor nulla.
+            Nam sed mauris felis.</p>
+        <i class="fas fa-reply" title="Réservé aux restaurateurs"></i>
+    </div>
 </div>
