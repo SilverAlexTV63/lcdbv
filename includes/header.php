5456551076e1900e4451efa4b3d51d63089947e9
root <root@Portable-Alex>
2020-06-02 00:24:09 +0200

    Ajout qr.php & includes/qr.php & assets/img/qr.png

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
             <img src="assets/img/long-logo.png">
         </a>
         <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navcol-1">
+        <div class="collapse navbar-collapse" id="navcol-1" style="justify-content: flex-end;">
             <form class="form-inline" target="_self">
                 <div class="form-group">
                     <input class="form-control search-field" type="search" id="search-field" name="search" style="background-color: rgba(255, 153, 102, .01); border-color: #990000;">
@@ -17,8 +17,9 @@
                 </div>
             </form>
             <ul class="nav navbar-nav">
-                <li class="nav-item" role="presentation"><a class="nav-link" href="index.php" style="color: black;">Accueil</a>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="index.php" style="color: black;">Accueil</a></li>
                 <li class="nav-item" role="presentation"><a class="nav-link" href="contact.php" style="color: black;">Contact</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="qr.php" style="color: black;">Menu QR</a><li>
                 <li class="nav-item" role="presentation"><a class="btn btn-primary ml-auto" role="button" href="connexion.php" style="border-color: #990000; background-color: #ff9966;">Inscription / Connexion</a></li>
             </ul>
         </div>
---
+++ qr.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include("includes/head.php"); ?>   
+    <?php include("includes/scripts.php"); ?>
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+    <?php include("includes/qr.php"); ?>
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
